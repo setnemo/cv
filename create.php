d426b809edd2704f8e19a1de5e90739c492c735a
Artem Pakhomov <apakhomov@blockster.com>
2021-07-31 20:23:14 +0300

    refactoring cv

--- create.php
+++ create.php
@@ -4,15 +4,21 @@ require __DIR__ . '/vendor/autoload.php';
 $mpdf = new \Mpdf\Mpdf([
     'tempDir' => '/tmp',
     'format' => 'A4',
-    'margin_left' => 7,
-    'margin_right' => 7,
-    'margin_top' => 5,
-    'margin_bottom' => 5,
+    'margin_left' => 9,
+    'margin_right' => 9,
+    'margin_top' => 9,
+    'margin_bottom' => 9,
     'margin_header' => 0,
     'margin_footer' => 0,
 ]);
 
 $html = file_get_contents('index.html');
+
+$html = strtr($html, [
+    'experience_years' => (new NumberFormatter("en", NumberFormatter::SPELLOUT))
+                                ->format(((new \DateTime('2015-05-01'))->diff(new \DateTime()))->format('%Y'))
+]);
+
 $mpdf->WriteHTML($html);
 
 $mpdf->Output('PHP_Pakhomov_CV.pdf', 'F');
